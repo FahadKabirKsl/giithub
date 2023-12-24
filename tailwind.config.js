f712e84bd92970384257919a0adcc7590fae3222
FahadKabirKsl <fahadkabirsezan@gmail.com>
2023-12-25 00:13:10 +0600

    github

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,104 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    typography: (theme) => ({
+      default: {
+        css: {
+          a: {
+            color: theme("colors.blue.600"),
+          },
+          img: {
+            display: "inline-block",
+          },
+          hr: {
+            borderColor: theme("colors.gray.400"),
+            marginTop: "2em",
+            marginBottom: "2em",
+          },
+        },
+      },
+    }),
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "meetup-blue": "#00455D",
+        "meetup-purple": "#1F24CC",
+        "gray-750": "#3f495a",
+        "gray-850": "#222733",
+        "gray-900-spotify": "#121212",
+        "gray-800-spotify": "#181818",
+        "gray-700-spotify": "#282828",
+      },
+      spacing: {
+        14: "3.5rem",
+        22: "5.5rem",
+        72: "18rem",
+        200: "50rem",
+      },
+      width: {
+        "7/10": "70%",
+        "3/10": "30%",
+      },
+      fontSize: {
+        xxs: "0.5rem",
+      },
+      lineHeight: {
+        "extra-loose": "2.5",
       },
     },
+    container: {
+      padding: "1rem",
+    },
+    fontFamily: {
+      sans: [
+        "-apple-system",
+        "BlinkMacSystemFont",
+        '"Segoe UI"',
+        "Roboto",
+        '"Helvetica Neue"',
+        "Arial",
+        '"Noto Sans"',
+        "sans-serif",
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+      "source-sans-pro": [
+        "Source Sans Pro",
+        "Roboto",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        '"Segoe UI"',
+        '"Helvetica Neue"',
+        "Arial",
+        '"Noto Sans"',
+        "sans-serif",
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+      serif: ["Georgia", "Cambria", '"Times New Roman"', "Times", "serif"],
+      mono: [
+        "Menlo",
+        "Monaco",
+        "Consolas",
+        '"Liberation Mono"',
+        '"Courier New"',
+        "monospace",
+      ],
+    },
   },
-  plugins: [],
-}
+  variants: {
+    textColor: ["responsive", "hover", "focus", "group-hover"],
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
